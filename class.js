5c22e9a9889de7cf743640380f9e7c6d7584ec12
Le Tuan Tai <tuantai568@gmail.com>
2018-12-11 23:15:47 +0700

    update hw 1

--- class.js
+++ class.js
@@ -1,6 +1,7 @@
 const listStudents = [];
 
 class Student {
+
   static addStudent(name, age, isFemale) {
     if (name !== '' && (undefined !== age && Number.isInteger(age)) && typeof(isFemale) === 'boolean') {
       return listStudents.push({
@@ -23,21 +24,34 @@ class Student {
 
   static getStudentByName(name) {
     if (listStudents.length > 0) {
-      listStudents.map(student => console.log(student))
+      listStudents.map(function(student) {
+        if (student.name === name) {
+          console.log(student);
+        }
+      });
     }
   }
 
-  static deleteStudentByName() {
-
+  static deleteStudentByName(name) {
+    if (listStudents.length > 0) {
+      for( var i = 0; i < listStudents.length; i++) { 
+        if ( listStudents[i].name === name) {
+         listStudents.splice(i, 1); 
+        }
+      }
+    }
   }
+
 }
 
 Student.addStudent('Tony', 24, true);
-Student.addStudent('Tony', 24, true);
+Student.addStudent('e', 24, true);
+Student.addStudent('d', 24, true);
+Student.addStudent('c', 20, true);
+Student.addStudent('b', 24, true);
+Student.addStudent('a', 24, true);
 
-Student.addStudent('Tony', 24, true);
-Student.addStudent('Tony', 20, true);
-Student.addStudent('Tony', 24, true);
-Student.addStudent('Tony', 24, true);
+Student.getStudentByName('a');
 
+Student.deleteStudentByName('Tony');
 console.log(listStudents);
